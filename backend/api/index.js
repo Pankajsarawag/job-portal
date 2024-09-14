0df724017c01cf3d4ff43c3c03082d2c6732a738
Jaya <vashisthjaya00@gmail.com>
2024-09-14 16:40:33 +0530

    api

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,10 +2,10 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRoute from "./routers/userRoutes.js";
-import companyRoute from "./routers/companyRoutes.js";
-import jobRoute from "./routers/jobRoutes.js";
-import applicationRoute from "./routers/applicationRoutes.js";
+import userRoute from "../routers/userRoutes.js";
+import companyRoute from "../routers/companyRoutes.js";
+import jobRoute from "../routers/jobRoutes.js";
+import applicationRoute from "../routers/applicationRoutes.js";
 dotenv.config({});
 
 const app = express();
